db27592633890e1e86af1cb70424966a61b12e94
shivambalwani <71224988+shivambalwani@users.noreply.github.com>
2021-01-02 20:54:10 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <ul>
+      <li>Gaming</li>
+      <li>Study</li>
+      <li>Web Dev</li>
+    </ul>
+  </div>,
+
+  rootElement
+);
